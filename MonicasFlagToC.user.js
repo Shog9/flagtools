6c502b8d05464cf2b03041d94b4123b3556bec59
Joshua Heyer <shog@stackoverflow.com>
2019-02-20 18:51:30 -0700

    more classes changing (ノಠ益ಠ)ノ彡┻━┻

--- MonicasFlagToC.user.js
+++ MonicasFlagToC.user.js
@@ -3,7 +3,7 @@
 // @description   Implement https://meta.stackexchange.com/questions/305984/suggestions-for-improving-the-moderator-flag-overlay-view/305987#305987
 // @author        Shog9
 // @namespace     https://github.com/Shog9/flagfilter/
-// @version       0.910
+// @version       0.911
 // @include       http*://stackoverflow.com/questions/*
 // @include       http*://*.stackoverflow.com/questions/*
 // @include       http*://dev.stackoverflow.com/questions/*
@@ -239,7 +239,7 @@ function initStyles()
       clear: both;
    }
    
-   .comment .comment-delete
+   .comment .js-comment-delete
    {
       float: right;      
    }
